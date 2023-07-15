26c3a266b378c85f7d73511ad985d204c16c1531
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-14 23:37:45 -0300

    criando Dao generic, metodo salvar e pesquisar

---
+++ src/main/java/posjavamavenhibernate/dao/DaoGeneric.java
@@ -0,0 +1,30 @@
+package posjavamavenhibernate.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import posjavamavenhibernate.HibernateUtil;
+
+public class DaoGeneric <E>{
+	private EntityManager entityManager = HibernateUtil.getEntityManager();
+	
+	public void salvar(E entidade) {
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(entidade);
+		transaction.commit();
+		
+	}
+	
+	public E pesquisar(E entidade) {
+		Object id = HibernateUtil.getPrimaryKey(entidade);
+		E e = (E) entityManager.find(entidade.getClass(),id);
+		return e;
+		
+	}
+	
+	public E pesquisar(Long id, Class<E> entidade) {
+		E e =  (E) entityManager.find(entidade, id);
+		return e;
+	}
+}
--- src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -2,10 +2,43 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import posjavamavenhibernate.dao.DaoGeneric;
+import posjavamavenhibernate.model.UsuarioPessoa;
+
 public class TesteHibernate {
 	
 	@Test
 	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setLogin("teste4");
+		pessoa.setNome("natanael ");
+		pessoa.setSenha("1234");
+		pessoa.setSobrenome("santos");		
+		daoGeneric.salvar(pessoa);
+		
+	}
+	@Test
+	public void testeBuscar() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		pessoa.setId(2L);
+		
+		pessoa = daoGeneric.pesquisar(pessoa);
+		
+		System.out.println(pessoa);
+		
+
+
+	}
+	@Test
+	public void testeBuscar2() {
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = daoGeneric.pesquisar(3L, UsuarioPessoa.class);
+				
+		System.out.println(pessoa);
+		
+
+
 	}
 }
